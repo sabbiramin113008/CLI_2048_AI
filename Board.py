92941ece0a8a5be270730743958f3600adbf508d
unknown <sabbiramin.cse11ruet@gmail.com>
2018-12-16 00:20:11 +0600

    Adding Printing Dilemma Finale

--- Board.py
+++ Board.py
@@ -15,19 +15,24 @@ class CheckerBoard:
         self.boards = boards
         self.template_board = template_board
 
-
     def print_board(self, final_score=None):
+        print("-------2048-----------\n")
+        print("----------------------------")
         for row in range(0, 4):
+            print ("|      |      |      |      |")
             row_printer = ' '
             for column in range(0, 4):
                 if self.boards[row][column] == 0:
-                    cell_print = "    "
+                    cell_print = "      "
                 else:
-                    cell_print = str(self.boards[row][column]).center(4)
+                    cell_print = str(self.boards[row][column]).center(6)
                 if column == 3:
-                    row_printer = "{}{}".format(row_printer, cell_print)
+                    row_printer = "{}{}|".format(row_printer, cell_print)
                 else:
                     row_printer = "{}{}{}".format(row_printer, cell_print, "|")
             print(row_printer)
+            print("|      |      |      |      |")
             if row < 3:
-                print("--------------------")
+                print("----------------------------")
+        print("----------------------------")
+
